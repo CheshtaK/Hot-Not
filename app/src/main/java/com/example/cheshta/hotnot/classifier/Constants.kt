0624aefae03a20fdafe679eac8c493fdd6f0f20b
CheshtaK <32400008+CheshtaK@users.noreply.github.com>
2018-06-19 22:47:44 +0530

    adding necessary utils and uri

--- app/src/main/java/com/example/cheshta/hotnot/classifier/Constants.kt
+++ app/src/main/java/com/example/cheshta/hotnot/classifier/Constants.kt
@@ -1,11 +1,12 @@
 package com.example.cheshta.hotnot.classifier
 
-class Constants {
-    val GRAPH_FILE_PATH = "file:///android_asset/graph.pb"
-    val LABELS_FILE_PATH = "file:///android_asset/labels.txt"
+const val GRAPH_FILE_PATH = "file:///android_asset/graph.pb"
+const val LABELS_FILE_PATH = "file:///android_asset/labels.txt"
 
-    val GRAPH_INPUT_NAME = "input"
-    val GRAPH_OUTPUT_NAME = "final_result"
+const val GRAPH_INPUT_NAME = "Placeholder"
+const val GRAPH_OUTPUT_NAME = "final_result"
 
-    val IMAGE_SIZE = 224
-}
+const val IMAGE_SIZE = 224
+const val COLOR_CHANNELS = 3
+
+const val ASSETS_PATH = "file:///android_asset/"
